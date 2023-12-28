20e3fb4aaec0c1eb193be7ce904a7a869fd6b1ad
Vivek Vaish <vaishvivek634@gmail.com>
2023-12-28 13:25:26 +0530

    temporary navbar completed

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,5 +1,19 @@
-import React from 'react'
+"use client"
+
+import { auth } from '@/config/firebase'
+import { onAuthStateChanged } from 'firebase/auth'
+import { useRouter } from 'next/navigation'
+import React, { useEffect } from 'react'
 const Dashboard = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        router.push('login');
+      }
+    })
+  })
   return (
       <div>Dashboard</div>
   )
---
+++ components/Navbar.jsx
@@ -0,0 +1,47 @@
+"use client";
+
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
+import MenuSVG from "@/public/menu.svg"
+import Image from 'next/image';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '@/config/firebase';
+
+const Navbar = () => {
+    const [navbar, setNavbar] = useState(false);
+    const [isAuth, setIsAuth] = useState(false);
+    const handleNavbar = () => {
+        setNavbar(!navbar)
+    }
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setIsAuth(true);
+            }
+        })
+    })
+    return (
+        <header className='p-5'>
+            <nav className='relative w-full bg-blue-800 h-[5rem] flex p-5 m-auto justify-between text-center items-center text-white'>
+                <div className="text-xl"><Link href='/'>Workflow</Link></div>
+                <ul className={`${navbar ? 'opacity-100' : 'opacity-0'} absolute top-[5rem] bg-yellow-500 w-full md:opacity-100 md:flex md:static md:w-fit md:bg-transparent`}>
+                    {!isAuth && <li className='flex flex-col p-6'>
+                        <Link onClick={() => { handleNavbar() }} href='/signup'>Sign Up</Link>
+                    </li>}
+                    {!isAuth && <li className='flex flex-col p-6'>
+                        <Link onClick={() => { handleNavbar() }} href='/login'>Login</Link>
+                    </li>}
+                    {isAuth && <li className='flex flex-col p-6'>
+                        <Link onClick={() => { handleNavbar() }} href='/dashboard'>Dashboard</Link>
+                    </li>}
+                </ul>
+                <div className="md:hidden">
+                    <Image onClick={() => handleNavbar()} className='cursor-pointer ' alt='menu' height={50} width={50} src={MenuSVG} />
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
