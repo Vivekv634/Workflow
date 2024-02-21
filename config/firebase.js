f7a380f4ec71509eb67499825956ead96f54f07f
vivek Vaish <vaishvivek634@gmail.com>
2024-02-22 00:00:06 +0530

    changing firebase config to public

--- config/firebase.js
+++ config/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { collection, getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
+    apiKey: "AIzaSyC-q6pTa2lST7G1g8KGshrGja2EFzDlJM0",
+    authDomain: "workflow-9f4ff.firebaseapp.com",
+    projectId: "workflow-9f4ff",
+    storageBucket: "workflow-9f4ff.appspot.com",
+    messagingSenderId: "479413635993",
+    appId: "1:479413635993:web:7ee27390ee697ad03be108",
+    measurementId: "G-1NWFSCR34F"
 };
 
 const app = initializeApp(firebaseConfig);
